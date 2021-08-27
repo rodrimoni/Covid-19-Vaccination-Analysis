256d8c085f5bcaf496fc1a0d44282e068e555646
rodrimoni <rodrimoni@gmail.com>
2021-08-27 20:06:05 -0300

    Minor fixes

--- mapa-covid.R
+++ mapa-covid.R
@@ -10,15 +10,16 @@ rs <- read_municipality(code_muni= "RS", year=2020)
 dados_vac <- read_csv("~/Mapa RS - Vacinação/dados_vac.csv")
 dados_vac <- dados_vac %>% separate('municipio', c("cod_municipio", "nome_municipio"), sep =" - ")
 dados_vac <- dados_vac %>% separate('porcent_esquema_vacinal_completo', c("porcent_esquema_vacinal_completo", "numero_bruto_esquema_vacinal_completo"), sep ="% ")
+dados_vac$porcent_esquema_vacinal_completo <- as.numeric(dados_vac$porcent_esquema_vacinal_completo)
 
 rs$code_muni <- substr(rs$code_muni,1,6)
 dataset_final = left_join(rs, dados_vac, by=c("code_muni"= "cod_municipio"))
 
 map <- st_as_sf(dataset_final)
 map <- st_transform(map, "+init=epsg:4326")
-bins <- c(0, 0.5, 0.8, 1, 1.3, 2, 7, 10)
-custom <- c("#3498DB", "#61C2E9", "#D6EAF8", "#FADBD8", "#FA9999", "#F25C5C", "#EE3D3D")
-pal <- colorBin(palette = custom, domain = map$smr, bins = bins)
+bins <- c(10, 30, 50, 70, 90)
+custom <- c('#ffffd4','#fed98e','#fe9929','#d95f0e','#993404')
+pal <- colorBin(palette = custom, domain = map$porcent_esquema_vacinal_completo, bins = bins)
 
 labels <- sprintf(
   "<strong>%s</strong><br/>Esquema vacinal completo em: %s",
@@ -28,7 +29,7 @@ labels <- sprintf(
 leaflet(map) %>%
   addTiles() %>%
   addPolygons(
-    #fillColor = ~ pal(smr),
+    fillColor = ~ pal(porcent_esquema_vacinal_completo),
     weight = 0.5,
     opacity = 1,
     color = "gray",
@@ -45,7 +46,7 @@ leaflet(map) %>%
       style = list("font-weight" = "normal", padding = "3px 8px"),
       textsize = "15px",
       direction = "auto")
-  ) #%>%
-  #addLegend(pal = pal, values = ~smr, opacity = 1, 
-   #         position = "bottomright",
-    #        title = "SMR por câncer de mama 2019")
+  ) %>%
+  addLegend(pal = pal, values = ~porcent_esquema_vacinal_completo, opacity = 1, 
+            position = "bottomright",
+            title = "Esquema Vacinal completo Covid 2021 no RS (%)")
